0c4a3de0d735f93a1ea207c297967dcf78029b8a
emanu <emanu@RedmiBook>
2026-05-29 10:38:08 -0500

    Aparentementeeee, se ha concluido el proyecto

--- src/main/java/proyecto/main.java
+++ src/main/java/proyecto/main.java
@@ -9,56 +9,77 @@ public class main {
     
     
     public static void main(String[] args){
-        
+        //Ruta para cargar los usuarios,
         String rutaCSV = "genz_dataset.csv";
+        //Ingreso de usuarios en arreglo como objetos
         usuario[] usuarios = obtenerUsuarios(rutaCSV);
-        // Convertir arreglo a ArrayList
+        //Conversión de array a ArrayList
         ArrayList<usuario> listaUsuarios = new ArrayList<>();
-
         for(usuario u : usuarios){
             if(u != null){
                 listaUsuarios.add(u);
             }
         }
-        for(int i=0; i<=10; i++){
-            System.out.println(usuarios[i].toString());
-        }   
-       
-        // Llamar al merge sort
+        
+        System.out.println("--- ORDENAMIENTOS ---");
+        
+        //Llamada a merge sort
+        System.out.println("\n--- INICIO DE ORDENAMIENTO POR MERGE SORT ---");
         MergeSort.mergeSort(listaUsuarios, 0, listaUsuarios.size()-1);
-
+        System.out.println("ORDENAMIENTO POR MERGE SORT EXITOSO!");
         // Mostrar despues de ordenar
-        System.out.println("\nDESPUES DEL MERGE SORT:");
-
+        System.out.println("--- RESULTADOS DE MERGE SORT ---");
+        System.out.println("Los datos fueron ordenados ascendentemente"
+                + " utilizando el valor, en horas, del uso en la noche de"
+                + " redes sociales por parte de los usuarios");
         for(int i = 0; i < 100; i++){
             System.out.println(listaUsuarios.get(i));
         }
         
-        System.out.println("\n--- ESTADISTICA USO NOCHE ---");
-        //Segunda estadistica top 10 usuarios con alto uso nocturno
-        Estadisticas.topPromedioUsoNoche(listaUsuarios);
-        
-        // Llamar al QuickSort para ordenar por promedioSesion
+        //Llamada a quick sort
+        System.out.println("\n--- INICIO DE ORDENAMIENTO POR QUICK SORT ---");
         quickSort.ordenar(listaUsuarios, 0, listaUsuarios.size() - 1);
-
-        // Mostrar despues de ordenar
-        System.out.println("\nDESPUES DEL QUICKSORT (Ordenado por Promedio de SesiOn):");
+        System.out.println("ORDENAMIENTO POR QUICK SORT EXITOSO!");
+        //Mostrar despues de ordenar
+        System.out.println("--- RESULTADOS DE QUICK SORT ---");
+        System.out.println("Los datos fueron ordenados ascendentemente"
+                + " utilizando el valor, en minutos, del promedio de duracion"
+                + " de sesion en redes sociales por parte de los usuarios");
         for(int i = 0; i < 100; i++){
          System.out.println(listaUsuarios.get(i));
         }
-          System.out.println("\n--- ESTADISTICA PROMEDIO SESION ---");
-        //Segunda estadistica top 10 usuarios 
-        Estadisticas.topPromedioSesion(listaUsuarios);
         
-        System.out.println("----");
-        //Hash
+        //Inicio del proceso Hashing
+        System.out.println("\n--- INICIO DEL HASHING ---");
+        System.out.println("PROCESO DE HASHING EXITOSO!");
+        System.out.println("--- RESULTADOS TABLA HASH ---");
         usuario[] tablaHash = new usuario[10357];
         for(usuario u: usuarios){
             insertarUsuario(u, tablaHash);
         }
-        
+        //Mostrar despues de insertar
         for(int i=0; i<100; i++){
-            System.out.println(tablaHash[i].getPromedioSesion()+" "+ tablaHash[i].getGenero());
+            System.out.println(tablaHash[i].getPromedioSesion()+" "+ tablaHash[i].getGenero()+
+            " " + tablaHash[i].getCountry());
         }
+        
+        //Estadísticas
+        System.out.println("\n--- ESTADISTICAS ---");
+        System.out.println("--- ESTADISTICA PROMEDIO SESION ---");
+        //Primera estadistica top 10 usuarios con el mayor promedio de uso por sesión
+        System.out.println("El siguiente top corresponde a los 10 usuarios que"
+                + " mas han utilizado redes sociales por sesion, medido en minutos");
+        Estadisticas.topPromedioSesion(listaUsuarios);
+        
+        System.out.println("\n--- ESTADISTICA USO NOCHE ---");
+        //Segunda estadistica top 10 usuarios con alto uso nocturno
+        System.out.println("El siguiente top corresponde a los 10 usuarios que"
+                + " mas han utilizado redes sociales en la noche, medido en horas");
+        Estadisticas.topPromedioUsoNoche(listaUsuarios);
+        
+        //Tercera estadística proporcion que usa redes sociales para fines informativos
+        System.out.println("\n--- PROPORCION DE USUARIOS, CASO ESPECIFICO ---");
+        Estadisticas.usuariosSegunProposito(usuarios);
+        
     }
 }
